6781752f7e46f3c07ba0e579439de3942a0cfe9b
Hassan Minhas <hassan.ahmad@confiz.com>
2024-12-09 21:07:26 +0500

    username/password update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,12 +5,12 @@ describe('login page test', () => {
   
       // Find an input username field, type into it, and verify the value
       cy.get('#UserLogin_username')
-        .type('hassan.ahmad@confiz.com')
-        .should('have.value', 'hassan.ahmad@confiz.com');
+        //.type('insert username')
+        //.should('have.value', 'insert username');
 
         cy.get('#UserLogin_password')
-        .type('ecb257ea460c9308f68cbcffc66d3cfd')
-        .should('have.value', 'ecb257ea460c9308f68cbcffc66d3cfd');
+       // .type('insert password')
+        //.should('have.value', 'insert password');
         
         cy.get('#yw0')
         .click();
